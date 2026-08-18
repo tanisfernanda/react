a326cd11a2a19821c14e8440d4d63336aed07f43
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-18 09:57:18 -0500

    cuarto intento

--- sena-react-ficha3409609/catalogo-react/src/App.jsx
+++ sena-react-ficha3409609/catalogo-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Producto from './components/Producto'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -115,8 +115,12 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+      <Producto />
     </>
   )
 }
 
 export default App
+    
