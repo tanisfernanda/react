7605701fd66bf48a306438b3289393da6a7bbd78
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-18 09:08:22 -0500

    crear componente producto

---
+++ sena-react-ficha3409609/catalogo-react/src/components/Producto.jsx
@@ -0,0 +1,10 @@
+export default function Producto() {
+  return (
+    <article className="producto">
+      <img src="/producto-demo.png" alt="Producto" />
+      <h2>Labial mate</h2>
+      <p>Producto para catálogo de belleza.</p>
+      <strong>$18.000</strong>
+    </article>
+  );
+}
