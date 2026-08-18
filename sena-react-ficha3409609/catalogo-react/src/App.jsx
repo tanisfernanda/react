524f1489488411a7206ef817c3cad8e4bfcb3ef0
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-18 10:06:47 -0500

    quinto intento

--- sena-react-ficha3409609/catalogo-react/src/App.jsx
+++ sena-react-ficha3409609/catalogo-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import Producto from './components/Producto'
 function App() {
   const [count, setCount] = useState(0)
 
