199a55a87273e7f45b06f1c8c608ac6635a26f6e
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-18 09:29:14 -0500

    tercer intento

--- sena-react-ficha3409609/catalogo-react/src/components/Producto.jsx
+++ sena-react-ficha3409609/catalogo-react/src/components/Producto.jsx
@@ -1,10 +1,12 @@
+import bonitaImagen from './bonita-imagen.jpg';
+
 export default function Producto() {
   return (
     <article className="producto">
-      <img src="bonita-imagen.jpn" alt="Produ-cto" />
+      <img src={bonitaImagen} alt="Producto" />
       <h2>Labial mate</h2>
       <p>Producto para catálogo de belleza.</p>
       <strong>$18.000</strong>
     </article>
   );
-}
+} 
