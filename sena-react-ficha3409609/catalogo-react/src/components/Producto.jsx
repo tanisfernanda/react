4cbecb627d4793448382afac651ce2a98fef892a
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-18 09:15:38 -0500

    intentano de poner la imagen

--- sena-react-ficha3409609/catalogo-react/src/components/Producto.jsx
+++ sena-react-ficha3409609/catalogo-react/src/components/Producto.jsx
@@ -1,7 +1,7 @@
 export default function Producto() {
   return (
     <article className="producto">
-      <img src="/producto-demo.png" alt="Producto" />
+      <img src="/bonita-imagen.jpn" alt="Produ-cto" />
       <h2>Labial mate</h2>
       <p>Producto para catálogo de belleza.</p>
       <strong>$18.000</strong>
